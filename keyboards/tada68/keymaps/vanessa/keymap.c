9ae08e80868089f7e54b7ef2599ff39407ad40e7
Vanessa Gregoriou <vanessagregoriou@github.com>
2018-04-20 11:22:56 +1000

    fix error in keymap

--- keyboards/tada68/keymaps/vanessa/keymap.c
+++ keyboards/tada68/keymaps/vanessa/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
   MO(_FL), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_HOME,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_END, \
-  KC_LCTRL, KC_LGUI,KC_LALT,                KC_SPC,                        MO(_FL),KC_RALT,,KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTRL, KC_LGUI,KC_LALT,                KC_SPC,                        MO(_FL),KC_RALT,KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = KEYMAP_ANSI(
   _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
-  _______, KC_BTN1, KC_MS_U,KC_BTN2,_______,_______, _______,_______,_______,KC_MS_WH_DOWN, KC_MS_WH_UP,_______, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK , KC_MEDIA_PLAY_PAUSE, KC_PGUP, \
+  _______, KC_BTN1, KC_MS_U,KC_BTN2,_______,_______,_______,_______,KC_MS_WH_DOWN, KC_MS_WH_UP,_______, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK , KC_MEDIA_PLAY_PAUSE, KC_PGUP, \
   _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,_______,KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,_______,_______,        _______, KC_PGDN, \
-  _______,_______,_______,________,_______,_______, _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
+  _______,_______,_______,_______,_______,_______,_______, _______,KC_VOLD,KC_VOLU,KC_MUTE,KC_BTN1, KC_MS_U, KC_BTN2, \
   _______,_______,_______,                 _______,               _______,_______,_______,KC_MS_L,KC_MS_D, KC_MS_R),
 };
